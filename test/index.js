64bb9fee595508628a385ea3025be93b8b402f28
designeng <denissavenok@gmail.com>
2019-05-30 13:22:13 +0300

    Init mocha tests

---
+++ test/index.js
@@ -0,0 +1,29 @@
+import polyfill from '@babel/polyfill'; /* for async/await support */
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+import Mocha from 'mocha';
+
+const mocha = new Mocha();
+
+const addSyncTestFiles = (dirs) => {
+    _.forEach(dirs, dir => {
+        fs.readdirSync(dir)
+            .filter(file => {
+                let arr = file.split('.');
+                let name = _.first(arr);
+                return _.last(arr) === 'js'
+                    && _.indexOf(['runner', 'index'], name) == -1;
+            })
+            .forEach(file => mocha.addFile(path.join(dir, file)));
+    });
+}
+
+addSyncTestFiles([
+    __dirname
+]);
+
+mocha.run(function(failures) {
+    process.exitCode = failures ? 1 : 0;
+    process.exit();
+});
