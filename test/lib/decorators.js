67fe57fef77ba3ea7e1949a93b545d76d4a09ec9
designeng <denissavenok@gmail.com>
2019-05-31 13:32:26 +0300

    Add test/lib/decorators.js - Use args decorator for component definition

---
+++ test/lib/decorators.js
@@ -0,0 +1,49 @@
+import { map, forEach, assign } from 'lodash';
+import { expect } from 'chai';
+import when from 'when';
+import { msleep } from 'sleep';
+
+import createContext from '../../src/lib/createContext';
+import args from '../../src/decorators/args';
+
+const Promise = when.promise;
+
+const spec = {
+    @args({$ref: 'C'}, {$ref: 'B'})
+    A: (c, b) => Promise(resolve => {
+        msleep(10);
+        resolve(c + b);
+    }),
+
+    @args()
+    B: () => Promise(resolve => {
+        msleep(10);
+        resolve('B');
+    }),
+
+    @args()
+    C: () => Promise(resolve => {
+        msleep(10);
+        resolve('C');
+    }),
+}
+
+describe('Use args decorator for component definition', async () => {
+    let context;
+
+    before(async function() {
+        try {
+            context = await createContext(spec);
+        } catch (error) {
+            console.log('ERROR:' , error);
+        }
+    });
+
+    it('context should be an object', () => {
+        expect(context).to.be.an('object');
+    });
+
+    it('context should have a component with value', () => {
+        expect(context.A).to.equal('CB');
+    });
+});
