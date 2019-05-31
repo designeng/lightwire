e49e2315988f76c67911e2930a622673a0720743
designeng <denissavenok@gmail.com>
2019-05-31 12:45:01 +0300

    Init tests in test/lib/normilizedSpec.js

---
+++ test/lib/normilizedSpec.js
@@ -0,0 +1,47 @@
+import { map, forEach, assign } from 'lodash';
+import { expect } from 'chai';
+import when from 'when';
+import { msleep } from 'sleep';
+
+import createContext from '../../src/lib/createContext';
+
+const Promise = when.promise;
+
+const spec = {
+    A: {
+        create: {
+            method: (b, c) => Promise(resolve => {
+                msleep(10);
+                resolve(b + c[0]);
+            }),
+            args: [
+                {$ref: 'B'},
+                {$ref: 'C'}
+            ]
+        }
+    },
+
+    B: 'B',
+
+    C: ['C']
+}
+
+xdescribe('Normalize spec components before wiring', async () => {
+    let context;
+
+    before(async function() {
+        try {
+            context = await createContext(spec);
+        } catch (error) {
+            console.log('ERROR:' , error);
+        }
+    });
+
+    it('context should be object', () => {
+        expect(context).to.be.an('object');
+    });
+
+    it('context should have c component with value', () => {
+        expect(context.A).to.equal('BC');
+    });
+});
