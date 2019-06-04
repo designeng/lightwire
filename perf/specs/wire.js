81b2b4dd2c92d21b0844c82201b25ebe46009122
designeng <denissavenok@gmail.com>
2019-06-05 00:13:38 +0300

    Use defer decorator to create more complex spec for wire.js perf test

--- perf/specs/wire.js
+++ perf/specs/wire.js
@@ -1,22 +1,52 @@
-export default {
-    someComponent: {
-        create: {
-            module: (d) => {
-                let arr = [];
-                for (var i = 0; i < 10; i++) {
-                    arr.push(`${i}_${d}`);
-                }
-                return arr;
-            },
-            args: [
-                {$ref: 'someDep'}
-            ]
-        }
-    },
+import args from '../../src/decorators/args';
+import { isArray } from 'lodash';
+import { mergeSpecs } from 'wire/lib/specUtils';
 
-    someDep: {
-        create: {
-            module: () => 'someDep'
+function defer(...config) {
+    return (target, name, description) => {
+        let spec;
+        if(isArray(config[0])) {
+            spec = mergeSpecs(config[0]);
+        } else {
+            spec = config[0];
+        }
+        return {
+            value: {
+                wire: {
+                    spec,
+                    defer: true,
+                    provide: config[1]
+                }
+            }
         }
     }
+}
+
+const firstSpec = {
+    @args()
+    first: () => 'first'
+}
+
+const secondSpec = {
+    @args()
+    second: () => 'second'
+}
+
+export default {
+    @args({$ref: 'someDep'})
+    someComponent: (someDep) => {
+        return someDep().then(ctx => {
+            let { first, second } = ctx;
+            ctx.destroy();
+
+            let arr = [];
+            for (var i = 0; i < 10; i++) {
+                arr.push(`${i}_${first}_${second}`);
+            }
+            return arr;
+        });
+    },
+
+    @defer([firstSpec, secondSpec])
+    someDep: {}
 }
