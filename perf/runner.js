9daa08f2b9b957352acef8de1c967d6e70c34ee0
designeng <denissavenok@gmail.com>
2019-06-04 12:18:17 +0300

    Move ./runner in perf folder

---
+++ perf/runner.js
@@ -0,0 +1,6 @@
+require('@babel/polyfill'); /* for async/await support */
+
+require('../starter');
+require('./lightwireBenchmark').default();
+// require('./wireEssentialBenchmark').default();
+// require('./wireBenchmark').default();
--- runner.js
+++
@@ -1,6 +0,0 @@
-require('@babel/polyfill'); /* for async/await support */
-
-require('./starter');
-require('./perf/lightwireBenchmark').default();
-// require('./perf/wireEssentialBenchmark').default();
-// require('./perf/wireBenchmark').default();
