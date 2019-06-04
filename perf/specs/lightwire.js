828c58e9863c12a9cee1ce9f458ffadc64315e1f
designeng <denissavenok@gmail.com>
2019-06-05 00:13:03 +0300

    Use defer decorator to create more complex spec

--- perf/specs/lightwire.js
+++ perf/specs/lightwire.js
@@ -1,15 +1,31 @@
 import args from '../../src/decorators/args';
+import defer from '../../src/decorators/defer';
+
+const firstSpec = {
+    @args()
+    first: () => 'first'
+}
+
+const secondSpec = {
+    @args()
+    second: () => 'second'
+}
 
 export default {
     @args({$ref: 'someDep'})
-    someComponent: (d) => {
-        let arr = [];
-        for (var i = 0; i < 10; i++) {
-            arr.push(`${i}_${d}`);
-        }
-        return arr;
+    someComponent: (someDep) => {
+        return someDep().then(ctx => {
+            let { first, second } = ctx;
+            ctx.destroy();
+
+            let arr = [];
+            for (var i = 0; i < 10; i++) {
+                arr.push(`${i}_${first}_${second}`);
+            }
+            return arr;
+        });
     },
 
-    @args()
-    someDep: () => 'someDep'
+    @defer([firstSpec, secondSpec])
+    someDep: {}
 }
