f43302c3dab3d7db145289b76e4494c82d2a6e6b
designeng <denissavenok@gmail.com>
2019-06-05 16:46:47 +0300

    Run express server in forked process. Add process.exit on SIGINT

--- test/lib/fixtures/toFork.js
+++ test/lib/fixtures/toFork.js
@@ -1,6 +1,17 @@
+var express = require('express');
+
 var pid = process.pid;
 process.send({ pid });
 
+var app = express();
+
+app.get('/', function (req, res) {
+    res.end('Forked process');
+});
+
+var server = app.listen(3002);
+
 process.on('SIGINT', () => {
-    console.log('CLOSE FORKED PROCESS.....', process.pid);
+    server.close();
+    process.exit();
 });
