7107eb398ea0cee491bffe3ca9f288f2581b7247
designeng <denissavenok@gmail.com>
2019-06-06 19:49:39 +0300

    Fix defer decorator logic

--- src/decorators/defer.js
+++ src/decorators/defer.js
@@ -1,13 +1,25 @@
-import createContext from '../lib/createContext';
+import createContext, { isRef } from '../lib/createContext';
+import { reduce, assign, union } from 'lodash';
 
-export default function defer(...specs) {
+export default function defer(specs, ...provide) {
     return (target, name, description) => {
         return {
             value: {
                 create: {
-                    module: () => (...args) => {
-                        return createContext.apply(null, specs);
-                    }
+                    module: (...resolved) => {
+                        let provideSpec = reduce(provide, (res, arg, index) => {
+                            if(isRef(arg)) {
+                                assign(res, {
+                                    [arg.$ref]: resolved[index]
+                                })
+                            }
+                            return res;
+                        }, {});
+                        let mergedSpecs = specs.concat(provideSpec);
+                        const callback = () => createContext(mergedSpecs);
+                        return callback;
+                    },
+                    args: provide
                 }
             }
         }
