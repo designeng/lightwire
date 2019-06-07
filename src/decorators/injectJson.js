54a648b83e5432370ec0d18b5c4ab1d7b1e89e52
designeng <denissavenok@gmail.com>
2019-06-07 15:40:16 +0300

    Add src/decorators/injectJson.js

---
+++ src/decorators/injectJson.js
@@ -0,0 +1,37 @@
+import createContext, { isRef } from '../lib/createContext';
+import { isArray, reduce, assign, union } from 'lodash';
+import when from 'when';
+
+export const RESPONSE_NOT_FOUND = 'Response component not found in specs';
+
+export default function injectJson(specs, ...provide) {
+    return (target, name, description) => {
+        const _specs = isArray(specs) ? specs : [specs]; /* normalize */
+
+        return {
+            value: {
+                create: {
+                    module: (...resolved) => {
+                        let provideSpec = reduce(provide, (res, arg, index) => {
+                            if(isRef(arg)) {
+                                assign(res, {
+                                    [arg.$ref]: resolved[index]
+                                })
+                            }
+                            return res;
+                        }, {});
+                        let mergedSpecs = _specs.concat(provideSpec);
+                        return when(createContext(mergedSpecs)).then(ctx => {
+                            if(ctx.response) {
+                                return ctx.response.json;
+                            } else {
+                                throw new Error(RESPONSE_NOT_FOUND);
+                            }
+                        });
+                    },
+                    args: provide
+                }
+            }
+        }
+    }
+}
