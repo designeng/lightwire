0d03ae464752936d597a92afa33946b70379ac8d
designeng <denissavenok@gmail.com>
2019-06-10 12:48:18 +0300

    Fix componentModule tests

--- test/component/componentModule.js
+++ test/component/componentModule.js
@@ -2,7 +2,6 @@ import { } from 'lodash';
 import { expect } from 'chai';
 
 const chai = require('chai');
-const spies = require('chai-spies');
 const sinonChai = require('sinon-chai');
 const sinon = require('sinon');
 
@@ -17,7 +16,7 @@ const ready = {
     }
 }
 
-xdescribe('ComponentModule invoke', async () => {
+describe('ComponentModule invoke', async () => {
 
     const func = (arg) => `some_${arg}`
 
@@ -42,34 +41,44 @@ xdescribe('ComponentModule invoke', async () => {
     it('Instance should have invoke method', () => {
         expect(componentModule.invoke).to.be.a('function');
     });
-
-    it('Instance should return value when invoked', () => {
-        expect(componentModule.invoke({$ref: 'one'})).to.equal('some_ONE_VALUE');
-    });
 });
 
 describe('ComponentModule invoke with complex $ref', () => {
-    const func = (prop) => {
-        return prop.replace(/_VALUE/, '');
-    }
+    const func = (...args) => args[0]
 
-    let componentModule;
+    let componentModule, spy;
 
     before(function() {
         try {
             componentModule = new ComponentModule(func, ready);
+            spy = sinon.spy(componentModule, 'func');
         } catch (error) {
             console.log('ERROR:' , error);
         }
     });
 
-    it('Instance invoke called with', () => {
-        var spy = sinon.spy(componentModule, 'func');
+    it('Instance invoke called with resolved arg', () => {
+        componentModule.invoke({$ref: 'one'});
+        expect(spy.calledWith(ready.one)).to.be.ok;
+    });
+
+    it('Instance invoke called with resolved arg', () => {
         componentModule.invoke({$ref: 'two.prop1'});
         expect(spy.calledWith(ready.two.prop1)).to.be.ok;
     });
 
+    it('Instance should return value when invoked', () => {
+        let res = componentModule.invoke({$ref: 'one'});
+        expect(res).to.equal('ONE_VALUE');
+    });
+
+    it('Instance two.prop1 should return value when invoked', () => {
+        let res = componentModule.invoke({$ref: 'two.prop1'});
+        expect(res).to.equal('PROP1_VALUE');
+    });
+
     after(() => {
+        componentModule.destroy();
         sinon.restore();
     })
 });
