69987b152a76bd07104016e24cf6ed538391eb44
designeng <denissavenok@gmail.com>
2019-06-14 17:57:08 +0300

    Add src/lib/index.js

---
+++ src/lib/index.js
@@ -0,0 +1,13 @@
+import createContext from './lib/createContext';
+
+import args from '../decorators/args';
+import defer from '../decorators/defer';
+import fork from '../decorators/fork';
+import injectJson from '../decorators/injectJson';
+
+export default createContext;
+
+export args;
+export defer;
+export fork;
+export injectJson;
