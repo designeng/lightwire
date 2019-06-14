465bfdf400169cffc55fdb8ed89644d17fff85e9
designeng <denissavenok@gmail.com>
2019-06-14 20:56:28 +0300

    Fix decorators/defer - provide simple values (not refs)

--- src/decorators/defer.js
+++ src/decorators/defer.js
@@ -4,20 +4,30 @@ import { map, reduce, forEach, assign, union, isArray, isObject, isString } from
 export default function defer(specs, ...provide) {
     return (target, name, description) => {
         const _specs = isArray(specs) ? specs : [specs]; /* normalize */
-        const renamed = {};
+        const customNames = new Map();
 
-        const realArgs = reduce(provide, (res, item, index) => {
+        let argsCount = 0;
+
+        const realArgs = reduce(provide, (res, item) => {
             let entries = Object.entries(item);
             if(entries.length > 1) {
                 forEach(entries, (entry, index) => {
                     let key = entries[index][0];
-                    let name = entries[index][1];
-                    if(key === '$ref' && isString(name)) {
-                        res.push({$ref: name});
+                    let t = entries[index][1];
+
+                    if(key === '$ref' && isString(t)) {
+                        res.push({$ref: t});
+                        argsCount++;
                     } else {
-                        renamed[index] = key;
-                        let _entries = Object.entries(name); /* name should be object here. TODO: test for $ref in _entries[0][0] */
-                        res.push({$ref: _entries[0][1]});
+                        customNames.set(argsCount, key);
+                        if(isObject(t) && t.hasOwnProperty('$ref')) {
+                            let _entries = Object.entries(t); /* name should be object here. TODO: test for $ref in _entries[0][0] */
+                            res.push({$ref: _entries[0][1]});
+                            argsCount++;
+                        } else {
+                            res.push(t);
+                            argsCount++;
+                        }
                     }
                 })
             } else if(entries.length === 1) {
@@ -25,9 +35,10 @@ export default function defer(specs, ...provide) {
                 let name = entries[0][1];
                 if(key === '$ref' && isString(name)) {
                     res.push({$ref: name});
+                    argsCount++;
                 }
             } else {
-                /* do nothing */
+                /* object is empty, do nothing */
             }
             return res;
         }, []);
@@ -37,12 +48,15 @@ export default function defer(specs, ...provide) {
                 create: {
                     module: (...resolved) => {
                         let provideSpec = reduce(realArgs, (res, arg, index) => {
+                            let customName = customNames.get(index);
                             if(isRef(arg)) {
-                                if(renamed[index]) {
-                                    assign(res, { [renamed[index]]: resolved[index] })
+                                if(customName) {
+                                    assign(res, { [customName]: resolved[index] })
                                 } else {
                                     assign(res, { [arg.$ref]: resolved[index] })
                                 }
+                            } else {
+                                assign(res, { [customName]: resolved[index] })
                             }
                             return res;
                         }, {});
