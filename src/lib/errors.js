de48b9a8bb4936d89cd13d7f5f486f8378db0852
designeng <denissavenok@gmail.com>
2019-09-04 14:05:51 +0300

    Add src/lib/errors.js

---
+++ src/lib/errors.js
@@ -0,0 +1,39 @@
+export function NotValidSpecError(message) {
+    this.constructor.prototype.__proto__ = Error.prototype;
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.code = 500;
+}
+
+export function ReservedNameError(message) {
+    this.constructor.prototype.__proto__ = Error.prototype;
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.code = 500;
+}
+
+export function NotDefinedComponentError(message) {
+    this.constructor.prototype.__proto__ = Error.prototype;
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.code = 500;
+}
+
+export function CyclesDetectedError(message) {
+    this.constructor.prototype.__proto__ = Error.prototype;
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.code = 500;
+}
+
+export function ComponentInvocationError(message) {
+    this.constructor.prototype.__proto__ = Error.prototype;
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.code = 500;
+}
